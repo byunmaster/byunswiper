066f823db8044a0889b761417f6c84870bb2c4e8
byunmaster <byunma@sk.com>
2020-01-23 23:06:50 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# byunswiper
+# Byun Swiper
+
+simple and light swiper for web.
+
+based on es5.
+
+## get started
+
+1. include `<script src="dist/byun.swiper.min.js"></script>` in your html.
+2. make a dom tree for swiper.(see example)
+3. call function `byunSwiper({ options });`
+
+### example
+
+```
+<div class="target">
+  <div class="container">
+    <div>Slide 1</div>
+    <div>Slide 2</div>
+    <div>Slide 3</div>
+  </div>
+</div>
+```
+
+```
+var swiper = byunSwiper({
+  target: document.querySelector('.target')
+});
+```
+
+## options
+
+| parameter | required |    type     | default | description              |
+| --------- | :------: | :---------: | :-----: | ------------------------ |
+| target    |    O     | HTMLElement |  null   | HTMLElement for swiper   |
+| multiple  |    X     |   number    |    1    | slide count per page     |
+| loop      |    X     |   boolean   |  false  | loop mode                |
+| speed     |    X     |   number    |   300   | transition duration (ms) |
+
+## methods
+
+| name   | description             |
+| ------ | ----------------------- |
+| prev() | swipe to previous slide |
+| next() | swipe to next slide     |
+
+## support
+
+except IE
---
+++ src/byun.swiper.js
@@ -0,0 +1,165 @@
+window.byunSwiper = function(_options) {
+  // option setting
+  var options = {
+    multiple: 1,
+    speed: 300,
+    loop: false
+  };
+  Object.keys(_options).forEach(function(key) {
+    options[key] = _options[key];
+  });
+
+  // fields
+  var $target = options.target;
+  if (!$target) {
+    throw 'target is null';
+  }
+  var $container = $target.firstElementChild;
+  var $slides = $container.children;
+  var currentPage = 1;
+  var isMoving = false;
+  var totalPage = Math.ceil($slides.length / options.multiple);
+  var slideWidth = 1;
+  var itemWidth = 0;
+  var pagesX = [];
+
+  // init
+  $target.style.overflow = 'hidden';
+  $container.style.display = 'inline-flex';
+
+  var fragment = document.createDocumentFragment();
+  for (var i = $slides.length, len = totalPage * options.multiple; i < len; i++) {
+    fragment.appendChild(document.createElement('div'));
+  }
+  $container.appendChild(fragment);
+
+  for (var i = 0, len = $slides.length; i < len; i++) {
+    $slides[i].setAttribute('data-idx', i);
+    $slides[i].style.textAlign = 'center';
+  }
+
+  if (options.loop) {
+    var prevFragment = document.createDocumentFragment();
+    var nextFragment = document.createDocumentFragment();
+    for (i = 0, len = options.multiple; i < len; i++) {
+      prevFragment.appendChild($slides[$slides.length - len + i].cloneNode(true));
+      nextFragment.appendChild($slides[i].cloneNode(true));
+    }
+    $container.insertBefore(prevFragment, $container.firstChild);
+    $container.appendChild(nextFragment);
+  }
+
+  // event binding
+  window.addEventListener('DOMContentLoaded', function() {
+    draw();
+    if (options.loop) {
+      goPage(currentPage);
+    }
+  });
+  window.addEventListener('resize', resize);
+  $container.addEventListener('transitionend', function(event) {
+    $container.style.transitionDuration = '0ms';
+    if (options.loop) {
+      if (currentPage > totalPage) {
+        goPage(1);
+      } else if (currentPage < 1) {
+        goPage(totalPage);
+      }
+    }
+    isMoving = false;
+  });
+
+  var timestamp = 0;
+  var touchX = 0;
+  $container.addEventListener('touchstart', function(e) {
+    touchX = e.touches[0].clientX;
+    timestamp = e.timeStamp;
+  });
+  $container.addEventListener('touchend', function(e) {
+    var distance = e.changedTouches[0].clientX - touchX;
+    var delay = e.timeStamp - timestamp;
+    if (delay < 1000 && Math.abs(distance) > 50) {
+      if (distance > 0) {
+        prev();
+      } else {
+        next();
+      }
+    } else {
+      movePage(currentPage);
+    }
+    timestamp = 0;
+    touchX = 0;
+  });
+  $container.addEventListener('touchmove', function(e) {
+    var x = pagesX[currentPage] + e.changedTouches[0].clientX - touchX;
+    $container.style.transform = 'translateX(' + x + 'px)';
+  });
+
+  // methods
+  function draw() {
+    slideWidth = $target.offsetWidth;
+    itemWidth = slideWidth / options.multiple;
+    for (var i = 0, len = $slides.length; i < len; i++) {
+      $slides[i].style.width = itemWidth + 'px';
+    }
+    pagesX = generatePagesX();
+  }
+  function resize(e) {
+    e.preventDefault();
+    draw();
+    if (options.loop) {
+      goPage(currentPage);
+    }
+  }
+  function generatePagesX() {
+    var pagesX = [];
+    if (options.loop) {
+      for (var i = 0; i <= totalPage + 1; i++) {
+        pagesX.push(-(i * slideWidth));
+      }
+    } else {
+      pagesX.push(0);
+      for (var i = 0; i < totalPage; i++) {
+        pagesX.push(-(i * slideWidth));
+      }
+    }
+    return pagesX;
+  }
+  function goPage(page) {
+    currentPage = page;
+    $container.style.transform = 'translateX(' + pagesX[page] + 'px)';
+  }
+  function movePage(page) {
+    isMoving = true;
+    currentPage = page;
+    $container.style.transitionDuration = options.speed + 'ms';
+    $container.style.transform = 'translateX(' + pagesX[page] + 'px)';
+    return {
+      canPrev: canPrev,
+      canNext: canNext
+    };
+  }
+  function prev() {
+    if (isMoving || !canPrev()) {
+      return movePage(currentPage);
+    }
+    return movePage(--currentPage);
+  }
+  function next() {
+    if (isMoving || !canNext()) {
+      return movePage(currentPage);
+    }
+    return movePage(++currentPage);
+  }
+  function canPrev() {
+    return options.loop || currentPage > 1;
+  }
+  function canNext() {
+    return options.loop || currentPage < totalPage;
+  }
+
+  return {
+    prev: prev,
+    next: next
+  };
+};
